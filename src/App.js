3e61474624749f2498bc95dfb3051121d84f9c6e
moheranus <danielshobe90@gmail.com>
2024-08-08 12:22:48 +0300

    new fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './components/pages/testpage/LandingPage';
+import AdminDashboard from './components/adminDashboard/AdminDashboard';
+import Layouts from './components/layout/Layouts';
+import ProtectedRoute from './protected/ProtectedRoute';
+import { AuthProvider } from './authcontext/AuthContext';
+import Booking from './components/pages/landingpage/booking/Booking';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <div className='app'>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/admin/dashboard"
+            element={
+              <ProtectedRoute role="admin">
+                <Layouts>
+                  <AdminDashboard />
+                </Layouts>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/user/dashboard"
+            element={
+              <ProtectedRoute role="user">
+                <LandingPage>
+                  <Booking/>
+                </LandingPage>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import Loader from './components/loaders/Loaders';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Index = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <App />
+        )}
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+
+createRoot(rootElement).render(<Index />);
